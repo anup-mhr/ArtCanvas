cb93d66135ec101deb4f981a795d1c62b56db817
Anup Maharjan <anupmhrzn16@gmail.com>
2024-05-08 21:58:44 +0545

    feat: add protecte route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
-
   return (
-    <>
-    <Button >hello</Button>
-    </>
-  )
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PrivateRoute component={<h1>Home</h1>} />}>
+            <Route
+              path="/dashboard"
+              element={<PrivateRoute component={<h1>dashboard</h1>} />}
+            />
+          </Route>
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/signup" element={<h1>signup</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
