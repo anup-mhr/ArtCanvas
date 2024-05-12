52571a3950c982420b5192d64cf5d98a9d0b4e9a
Anup Maharjan <anupmhrzn16@gmail.com>
2024-05-12 12:15:36 +0545

    add tooltips for tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -10,6 +10,12 @@ import {
   Trash2,
   WholeWord,
 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Button } from "./ui/button";
 import { Separator } from "@radix-ui/react-separator";
 
@@ -24,58 +30,162 @@ export default function Tools({
     <>
       <div className="absolute top-10 left-5">
         <div className="flex  flex-col space-y-4 border-2 p-2 rounded-lg shadow-md">
-          <Button variant="outline" size="icon" onClick={() => saveCanvas()}>
-            <Download />
-          </Button>
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setType("select")}
-          >
-            <Hand />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => saveCanvas()}
+                >
+                  <Download />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Download Canvas</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("select")}
+                >
+                  <Hand />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Move</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
           <Separator orientation="horizontal" />
 
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setType("rectangle")}
-          >
-            <RectangleHorizontal />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("rectangle")}
+                >
+                  <RectangleHorizontal />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Rectangle</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
-          <Button variant="outline" size="icon" onClick={() => setType("line")}>
-            <Minus />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("line")}
+                >
+                  <Minus />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Line</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
-          <Button variant="outline" size="icon" onClick={() => setType("text")}>
-            <WholeWord />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("text")}
+                >
+                  <WholeWord />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Text</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setType("pencil")}
-          >
-            <PencilLine />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("pencil")}
+                >
+                  <PencilLine />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Pencil</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => setType("circle")}
-          >
-            <Circle />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => setType("circle")}
+                >
+                  <Circle />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Circle</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
 
           <Separator orientation="horizontal" />
-          <Button variant="outline" size="icon" onClick={() => deleteElement()}>
-            <Trash2 />
-          </Button>
-          <Button variant="outline" size="icon" onClick={() => clearCanvas()}>
-            <Recycle />
-          </Button>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => deleteElement()}
+                >
+                  <Trash2 />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Delete Selected Element</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  onClick={() => clearCanvas()}
+                >
+                  <Recycle />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Clear Canvas</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
       <div className="text-center bg-gray-100 rounded-lg">
